9c94bddadc790468524fe4319236c658f28b2b9f
Alexandre Magno Monteiro <alexandre.magno@forlogic.net>
2018-08-10 17:40:24 -0300

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-5ab442fdad48dc10b638.js","c28f814fbd85b26327ae9603a56fbfbd"],["/app-b3dba92f2029683c6dcd.js","3cfd24a5b66ca2b3bb8f51094db3f5eb"],["/commons-0573d3c69a6704dd859d.js","9a12aec10c1c9cf2b3a0a431570696b8"],["/commons-89257bb4b41994efac2e.js","8940045568ca8309ee01a231c7f4af73"],["/index.html","01e4a54a2ca9cd8a0a85326efa1bb324"],["/offline-plugin-app-shell-fallback/index.html","5ac9212511dea0a1db78a69209fa118c"]];
+var precacheConfig = [["/app-5ab442fdad48dc10b638.js","c28f814fbd85b26327ae9603a56fbfbd"],["/app-b3dba92f2029683c6dcd.js","3cfd24a5b66ca2b3bb8f51094db3f5eb"],["/commons-0573d3c69a6704dd859d.js","9a12aec10c1c9cf2b3a0a431570696b8"],["/commons-89257bb4b41994efac2e.js","8940045568ca8309ee01a231c7f4af73"],["/index.html","c5a406d6be7acf6560bf12b6b769cf7b"],["/offline-plugin-app-shell-fallback/index.html","b9ed78aaca8cdc394aa14282d2214301"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
